fb526940f02c9ed0a60b7ab64fe5adb8956700ee
rudolf1118 <rud.haroutyunyan@gmail.com>
2025-05-03 01:55:05 +0400

    feat: improve error handling in authentication decorators with HttpException support

--- utils/decorator.js
+++ utils/decorator.js
@@ -1,14 +1,16 @@
-import authService from "../controller/auth.controller.js";
-import { handleResponse } from "./response.js";
+import authService from '../controller/auth.controller.js';
+import { handleResponse } from './response.js';
+import { HttpException } from './exceptions.js';
+
 class Decorator {
-    constructor() {
-    }
-    
+    constructor() {}
+
     async withAuth(req, res, handler, isRes = true) {
-            try {
-                await authService.checkToken_(req, res);
-                return (await handler(req, res));
-            } catch (error) {
+        try {
+            await authService.checkToken_(req, res);
+            return await handler(req, res);
+        } catch (error) {
+            /*
                 if (isRes) {
                     if (error) {
                         return handleResponse(res, 401, error.message);
@@ -16,21 +18,23 @@ class Decorator {
                     return handleResponse(res, 500, "Something went wrong");
                 }
                 throw error;
+                */
+            if (error instanceof HttpException) {
+                return handleResponse(res, error.status, error.message);
             }
+            return handleResponse(res, 500, 'Something went wrong');
+        }
     }
 
     async withBasicAuth(req, res, handler, isRes = true) {
         try {
             await authService.checkBasic(req, res);
-            return (await handler(req, res));
+            return await handler(req, res);
         } catch (error) {
-            if (isRes) {
-                if (error) {
-                    return handleResponse(res, 401, error.message);
-                }
-                return handleResponse(res, 500, "Something went wrong");
+            if (error instanceof HttpException) {
+                return handleResponse(res, error.status, error.message);
             }
-            throw error;
+            return handleResponse(res, 500, 'Something went wrong');
         }
     }
 }
